fad12ff50c85b57244dd8d285837023dcaeb05eb
tamar-mepe <tdavi18@freeuni.edu.ge>
2020-12-08 15:41:55 +0400

    add invite popup

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,51 @@
 window.onload = function () {
     let dropDownButton = document.querySelector('.plus-button');
     let menu = document.querySelector('.menu-content');
-    dropDownButton.addEventListener('click', () => {
-        if (menu.style.display === "") {
-            menu.style.display = "block";
-            dropDownButton.style.backgroundColor = "rgb(236, 236, 236)";
-        } else {
-            menu.style.display = "";
-            dropDownButton.style.backgroundColor = "white";
-        }
-    })
-    document.getElementById("join-class").addEventListener('click', () => {
-        document.getElementById("join-class-popup").style.visibility = "visible";
-    })
-    document.getElementById("close-join").addEventListener('click', () => {
-        document.getElementById("join-class-popup").style.visibility = "hidden";
-    })
-    document.getElementById("create-class").addEventListener('click', () => {
-        document.getElementById("create-class-popup").style.visibility = "visible";
-    })
-    document.getElementById("close").addEventListener('click', () => {
-        document.getElementById("create-class-popup").style.visibility = "hidden";
-    })
+    if (dropDownButton != null) {
+        dropDownButton.addEventListener('click', () => {
+            if (menu.style.display === "") {
+                menu.style.display = "block";
+                dropDownButton.style.backgroundColor = "rgb(236, 236, 236)";
+            } else {
+                menu.style.display = "";
+                dropDownButton.style.backgroundColor = "white";
+            }
+        })
+    }
+    let joinClassButton = document.getElementById("join-class");
+    if (joinClassButton != null) {
+        joinClassButton.addEventListener('click', () => {
+            document.getElementById("join-class-popup").style.visibility = "visible";
+        })
+    }
+    let closeJoinButton = document.getElementById("close-join");
+    if (closeJoinButton != null) {
+        document.getElementById("close-join").addEventListener('click', () => {
+            document.getElementById("join-class-popup").style.visibility = "hidden";
+        })
+    }
+    let createClassButton = document.getElementById("create-class");
+    if (createClassButton != null) {
+        createClassButton.addEventListener('click', () => {
+            document.getElementById("create-class-popup").style.visibility = "visible";
+        })
+    }
+    let closeCreateButton = document.getElementById("close");
+    if (closeCreateButton != null) {
+        closeCreateButton.addEventListener('click', () => {
+            document.getElementById("create-class-popup").style.visibility = "hidden";
+        })
+    }
+    let inviteButton = document.getElementById("invite-button");
+    if (inviteButton != null) {
+        inviteButton.addEventListener('click', () => {
+            document.getElementById("invite-popup-container").style.visibility = "visible";
+        })
+    }
+    let inviteClose = document.getElementById("invite-close-button");
+    if (inviteClose != null) {
+        inviteClose.addEventListener('click', () => {
+            document.getElementById("invite-popup-container").style.visibility = "hidden";
+        })
+    }
 };
